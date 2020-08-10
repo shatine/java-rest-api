82eaed41f24d6aac74dcacd026f1a4101db59be1
DESKTOP-81JT83A\jacksparrow <goldaladin@naver.com>
2020-08-11 00:35:16 +0900

    add commit push

---
+++ src/main/java/com/ourspace/studyjavarestapi/StudyJavaRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.ourspace.studyjavarestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJavaRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyJavaRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ourspace/studyjavarestapi/StudyJavaRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ourspace.studyjavarestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyJavaRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
