9ffc093e0e4bd27af8efcde0c11a5e1a4d4789c0
DESKTOP-81JT83A\jacksparrow <goldaladin@naver.com>
2020-08-17 17:23:17 +0900

    Create EventControllerTests.java
    lecture 11 : EventRepository 구현

---
+++ src/test/java/com/ourspace/studyjavarestapi/events/EventControllerTests.java
@@ -0,0 +1,75 @@
+package com.ourspace.studyjavarestapi.events;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class EventControllerTests {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    EventRepository eventRepository;
+
+    @Autowired
+    WebApplicationContext wac;
+
+    @Before
+    public void setUp() {
+        mockMvc = webAppContextSetup(wac).addFilter(((request, response, chain) -> {
+            response.setCharacterEncoding("UTF-8");
+            chain.doFilter(request, response);
+        })).build();
+    }
+
+    @Test
+    public void createEvent() throws Exception {
+        Event event = Event.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2020, 8, 17, 16, 10))
+                .closeEnrollmentDateTime(LocalDateTime.of(2020, 8, 17, 16, 11))
+                .beginEventDateTime(LocalDateTime.of(2020, 8, 18, 13, 0))
+                .endEventDateTime(LocalDateTime.of(2020, 8, 18, 15, 00))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타업 팩토리")
+                .build();
+
+        event.setId(8);
+        Mockito.when(eventRepository.save(event)).thenReturn(event);
+
+        mockMvc.perform(post("/api/events/")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaTypes.HAL_JSON)
+                    .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists());
+    }
+}
